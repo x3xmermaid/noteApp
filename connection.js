09f27c9b070d6edf3122aacdc6cf87c8002f3c75
x3xmermaid <32973695+x3xmermaid@users.noreply.github.com>
2019-06-17 23:38:26 +0700

    env

--- connection.js
+++ connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config()
+// const dotEn = require('dotenv');
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'noteApp',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 })
 
 con.connect(function(error){
