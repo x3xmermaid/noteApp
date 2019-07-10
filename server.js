38c75ea2b0a0f9cadea47f2cdf6a9e0df1dfba46
x3xmermaid <32973695+x3xmermaid@users.noreply.github.com>
2019-07-10 11:10:27 +0700

    fix first

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port =   process.env.port || 3001;
+const port =   process.env.PORT;
 const bodyParser = require('body-parser')
 const routes = require('./routes');
 const cors = require('cors');
