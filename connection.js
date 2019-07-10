41431221f94dbf1ae68de115cdf04207157b3359
x3xmermaid <32973695+x3xmermaid@users.noreply.github.com>
2019-07-10 10:54:45 +0700

    database connection

--- connection.js
+++ connection.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 // const dotEn = require('dotenv');
 
 const con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: "remotemysql.com",
+    user: "a2JUWyZwY9",
+    password: "6q1FkkdpSB",
+    database: "a2JUWyZwY9",
 })
 
 con.connect(function(error){
