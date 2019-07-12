85a3a76166b8b383ee91cce3ca140649308b3740
x3xmermaid <32973695+x3xmermaid@users.noreply.github.com>
2019-07-13 01:37:18 +0700

    fix manual cart

--- control/controllerManual.js
+++ control/controllerManual.js
@@ -8,14 +8,14 @@ exports.show = function(req, res){
                 `SELECT tb_product.id_store, tb_store.store_name, tb_store.location FROM tb_product 
                  INNER JOIN tb_cart on tb_cart.id_product=tb_product.id_product 
                  INNER JOIN tb_store on tb_store.id_store=tb_product.id_store 
-                 where tb_cart.id_user=1 group by tb_product.id_store`,
+                 where tb_cart.id_user=1 and tb_cart.buyed=1 group by tb_product.id_store`,
                 function(error, rows, field){
                     if(error){
                         throw error;
                     }else{
                         con.query(`SELECT * FROM tb_product
                                     INNER JOIN tb_cart on tb_cart.id_product=tb_product.id_product 
-                                    where tb_cart.id_user=1`,
+                                    where tb_cart.id_user=1 and tb_cart.buyed=1`,
                                 function(err, rows2, field){
                                     if(err){
                                         throw err
